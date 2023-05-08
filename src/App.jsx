2b23ff312d46de1d49476875fed9b661d8c5628c
Dimitar <dimikalapocev@gmail.com>
2023-05-08 15:51:17 +0200

    Add function to calculate and display tip and total

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,81 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [bill, setBill] = useState('');
+  const [tipPercent, setTipPercent] = useState('');
+  const [numPeople, setNumPeople] = useState('');
+  const [tipAmount, setTipAmount] = useState(0);
+  const [perPerson, setPerPersonAmount] = useState(0);
+  const [errorMsg, setErrorMsg] = useState('');
+
+  useEffect(() => {
+    const billAmount = parseFloat(bill);
+    const tipPercentAmount = parseFloat(tipPercent) / 100;
+    const numPeopleAmount = parseFloat(numPeople);
+    if (billAmount > 0 && tipPercentAmount > 0 && numPeopleAmount > 0) {
+      const tip = (billAmount * tipPercentAmount) / numPeople;
+      const perPerson = billAmount / numPeopleAmount + tip;
+
+      setTipAmount(tip);
+      setPerPersonAmount(perPerson);
+      setErrorMsg('');
+    } else {
+      setTipAmount(0);
+      setPerPersonAmount(0);
+      setErrorMsg(`Can't be zero`);
+    }
+  }, [bill, tipPercent, numPeople]);
+
   return (
     <div>
       <div className='container'>
         <div className='column'>
           <div>
             <label htmlFor='bill'>Bill</label>
-            <input type='number' id='bill' />
+            <input
+              type='number'
+              id='bill'
+              value={bill}
+              onChange={(e) => setBill(e.target.value)}
+            />
           </div>
           <div>
             <p>Select Tip %</p>
 
             <div className='flex-wrap'>
-              <button className='tip-btn' id='5'>
+              <button className='tip-btn' onClick={() => setTipPercent('5%')}>
                 5%
               </button>
-              <button className='tip-btn' id='5'>
+              <button className='tip-btn' onClick={() => setTipPercent('10%')}>
                 10%
               </button>
-              <button className='tip-btn' id='5'>
+              <button className='tip-btn' onClick={() => setTipPercent('15%')}>
                 15%
               </button>
-              <button className='tip-btn' id='5'>
+              <button className='tip-btn' onClick={() => setTipPercent('25%')}>
                 25%
               </button>
-              <button className='tip-btn' id='5'>
+              <button className='tip-btn' onClick={() => setTipPercent('50%')}>
                 50%
               </button>
-              <input type='number' id='custom-tip' />
+              <input
+                type='number'
+                value={tipPercent}
+                onChange={(e) => setTipPercent(e.target.value)}
+              />
             </div>
 
             <div>
               <label htmlFor='number-of-people'>Number of People</label>
-              <input type='number' id='number-of-people' />
+              <input
+                min='1'
+                type='number'
+                id='number-of-people'
+                value={numPeople}
+                onChange={(e) => setNumPeople(e.target.value)}
+              />
+              <p>{errorMsg}</p>
             </div>
           </div>
         </div>
@@ -43,14 +85,14 @@ function App() {
               <p>Tip Amount</p>
               <p>/ person</p>
             </div>
-            <p id='tip-amount'>$0.00</p>
+            <p id='tip-amount'>${tipAmount.toFixed(2)}</p>
           </div>
           <div className='flex-container'>
             <div>
               <p>Total</p>
               <p>/ person</p>
             </div>
-            <p id='total-amount'>$0.00</p>
+            <p id='total-amount'>${perPerson.toFixed(2)}</p>
           </div>
 
           <button>Reset</button>
